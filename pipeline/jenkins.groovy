295e3ea85c08a1a90209597ab6439aaf4b3bb88c
romanfeshchak <roman.feshchak@gmail.com>
2024-04-24 22:19:29 +0300

    edit groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,17 +1,14 @@
 pipeline {
     agent any
     parameters {
-
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-
+        choice(name: 'ARCH', choices: ['amd64', 'arm64', 'x86-64'], description: 'Pick architecture')
     }
     stages {
-        stage('Choose build parameters') {
+        stage('Choose build param') {
             steps {
                 echo "Build for platform ${params.OS}"
-
                 echo "Build for arch: ${params.ARCH}"
-
             }
         }
     }
